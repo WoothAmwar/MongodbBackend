3a07f763b127bc84bc0a87945ab75b6e76ef8928
Anwar <anwar09102005@gmail.com>
2022-07-26 17:47:02 -0400

    HTTPS

--- authenticate.js
+++ authenticate.js
@@ -44,3 +44,13 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }))
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});  // token based, not creating sessions
+exports.verifyAdmin = function(req, res, next) {
+    if (req.user.admin) {
+        return next();
+    }
+    else {
+        var err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    }
+};
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -21,7 +21,7 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.create(req.body)
     .then((dish) => {
         console.log("Dish Created ", dish);
@@ -31,11 +31,11 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes');
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.remove({})
     .then((resp) => {
         res.statusCode = 200;
@@ -56,12 +56,12 @@ dishRouter.route('/:dishId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403;
     // Can't post on a single item, since the single item exists so it has been filled.
     res.end("POST operation not supported on /dishes/" + req.params.dishId);
 })
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.findByIdAndUpdate(req.params.dishId, {
         $set: req.body
     }, { new: true })
@@ -72,7 +72,7 @@ dishRouter.route('/:dishId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.findByIdAndRemove(req.params.dishId)
     .then((resp) => {
         res.statusCode = 200;
@@ -134,7 +134,7 @@ dishRouter.route('/:dishId/comments')
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes/' + req.params.dishId + "/comments");
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null) {
@@ -190,9 +190,16 @@ dishRouter.route('/:dishId/comments/:commentId')
     // Can't post on a single item, since the single item exists so it has been filled.
     res.end("POST operation not supported on /dishes/" + req.params.dishId + '/comments/' + req.params.commentId);
 })
-.put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
+        if (req.user._id.toString() != dish.comments.id(req.params.commentId).author._id.toString()) {
+            console.log(req.user._id);
+            console.log(dish.comments.id(req.params.commentId).author._id);
+            err = new Error('You are not allowed to delete this comment!');
+            err.status = 403;
+            return next(err);
+        }
         // updates a subdocument within the document(comment)
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
             if (req.body.rating) {
@@ -225,9 +232,17 @@ dishRouter.route('/:dishId/comments/:commentId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
+        if (req.user._id.toString() != dish.comments.id(req.params.commentId).author._id.toString()) {
+            console.log(req.user._id);
+            console.log(dish.comments.id(req.params.commentId).author._id);
+            err = new Error('You are not allowed to delete this comment!');
+            err.status = 403;
+            return next(err);
+        }
+
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
             dish.comments.id(req.params.commentId).remove();
             dish.save()
